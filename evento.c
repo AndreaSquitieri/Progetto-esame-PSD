73f21fe6c7f405ebcc982c25e3a79cc8d886a3ef
Andeser-rgb <andeser@protonmail.com>
2024-04-12 21:10:08 +0200

    Added memory allocations checks and warning supp.

--- evento.c
+++ evento.c
@@ -13,7 +13,7 @@ typedef struct EventoStruct {
 Evento nuovo_evento(TipoEvento tipo, char *nome, Data data) {
   Evento nuovo_evento = calloc(1, sizeof(Evento_t));
   if (nuovo_evento == NULL) {
-    fprintf(stderr, "[ERRORE]: Allocazione oggetto 'evento' fallita.\n");
+    (void)fprintf(stderr, "[ERRORE]: Allocazione oggetto 'evento' fallita.\n");
     return NULL;
   }
   nuovo_evento->data = copia_data(data);
@@ -36,14 +36,27 @@ static const char *const stringhe_tipo_evento[] = {
 
 char *stringa_evento(Evento evento) {
   const char *str_tipo = stringhe_tipo_evento[evento->tipo];
+
   char *str_data = stringa_data(evento->data);
+  if (str_data == NULL) {
+    (void)fprintf(stderr,
+                  "[ERRORE]: Allocazione oggetto 'str_data' fallita.\n");
+    return NULL;
+  }
 
   int len = snprintf(NULL, 0, FORMAT_EVENTO, evento->nome, str_tipo, str_data);
+
   char *str_res = calloc(len + 1, sizeof(char));
+  if (str_res == NULL) {
+    (void)fprintf(stderr, "[ERRORE]: Allocazione oggetto 'str_res' fallita.\n");
+    return NULL;
+  }
+
   if (snprintf(str_res, len, FORMAT_EVENTO, evento->nome, str_tipo, str_data) <
       0) {
-    fprintf(stderr, "[ERRORE]: Creazione 'stringa evento' fallita.\n");
+    (void)fprintf(stderr, "[ERRORE]: Creazione 'stringa evento' fallita.\n");
     free(str_data);
+    free(str_res);
     return NULL;
   }
 
