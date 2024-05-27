30c91af0cfddfb0694bbda8ab84d782132545cff
Andrea <andeser@protonmail.com>
2024-05-27 22:35:26 +0200

    Cose

--- report/.auctex-auto/main.el
+++ report/.auctex-auto/main.el
@@ -3,13 +3,12 @@
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("scrartcl" "11pt")))
-   (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "structure"
     "scrartcl"
-    "scrartcl11"))
+    "scrartcl11"
+    "listings"
+    "xcolor"))
  :latex)
 
